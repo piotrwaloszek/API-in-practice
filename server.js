4be7cc6a0a445ec3927c5150e0de9c244acb95f7
Piotr Wałoszek <piotrek.waloszek@gmail.com>
2021-07-11 10:08:11 +0200

    Changes in server.js

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
 
@@ -7,6 +8,9 @@ const testimonialsRoutes = require('./routes/testimonials.routes');
 const concertsRoutes = require('./routes/concerts.routes');
 const seatsRoutes = require('./routes/seats.routes');
 
+app.use(express.static(path.join(__dirname, '/client/public')));
+
+app.use(cors());
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 app.use(cors());
@@ -14,9 +18,13 @@ app.use('/api', testimonialsRoutes);
 app.use('/api', concertsRoutes);
 app.use('/api', seatsRoutes);
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/client/public/index.html'));
+});
+
 app.use((req, res) => {
   res.status(404).send('404 not found...');
 })
-app.listen(8000, () => {
-    console.log('Server is running on port: 8000');
+app.listen(process.env.PORT || 8000, () => {
+  console.log('Server is running on port: 8000');
 });
