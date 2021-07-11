d5650bd587d2ea21d90ddf1cf6d0575aeb25699c
Piotr Wałoszek <piotrek.waloszek@gmail.com>
2021-07-11 10:23:49 +0200

    App ready to publish

--- server.js
+++ server.js
@@ -8,18 +8,16 @@ const testimonialsRoutes = require('./routes/testimonials.routes');
 const concertsRoutes = require('./routes/concerts.routes');
 const seatsRoutes = require('./routes/seats.routes');
 
-app.use(express.static(path.join(__dirname, '/client/public')));
-
 app.use(cors());
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
+app.use(express.static(path.join(__dirname, './client/build')));
 app.use(cors());
 app.use('/api', testimonialsRoutes);
 app.use('/api', concertsRoutes);
 app.use('/api', seatsRoutes);
-
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '/client/public/index.html'));
+  res.sendFile(path.join(__dirname, './client/build/index.html'));
 });
 
 app.use((req, res) => {
