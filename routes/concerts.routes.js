aea0cdeb4e4307e1770af80f62e9586b08cbb905
Piotr Wałoszek <piotrek.waloszek@gmail.com>
2021-07-15 17:00:38 +0200

    Remove if from routes

--- routes/concerts.routes.js
+++ routes/concerts.routes.js
@@ -13,36 +13,11 @@ router.route('/concerts/:id').get((req, res) => {
 });
 
 router.route('/concerts').post((req, res) => {
-    const {performer, genre, price, day, image} = req.body;
     req.body.id = uuidv4();
-
-    if (performer && genre && price && day){
-        performer;
-        genre;
-        price;
-        day;
-        image;
-        db.concerts.push(req.body);
-        res.json({message: 'OK'});
-    } else {
-        res.json({message: 'Please put all informations'});
-    }
 });
 
 router.route('/concerts/:id').put((req, res) => {
     const concert = db.concerts.find(item => item.id == req.params.id);
-    const {performer, genre, price, day, image} = req.body;
-    
-    if(performer && genre && price && day){
-        concert.performer = performer;
-        concert.genre = genre;
-        concert.price = price;
-        concert.day = day;
-        concert.image = image;
-        res.json({message: 'OK'});
-    } else {
-        res.json({message: 'Please put all informations'});
-    }
 });
 
 router.route('/concerts/:id').delete((req, res) => {
